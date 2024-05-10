a9823de61cbe9bdbf9edc5dacf613777d52da9d4
00doggie <47480725+00doggie@users.noreply.github.com>
2024-05-10 05:06:13 -0400

    Added Boiling Functionality. place campfire under kettle to make water boil added first version of bubble animation when isboiling is true added wip tea textures
    known bugs:
    does not check if water is in machine to display bubbles results in moving bubbles on air
    machine does not check for boiling water in order for processing.

---
+++ src/main/java/com/util/TickHandler.java
@@ -0,0 +1,19 @@
+package com.util;
+
+import com.herbalist.Herbalist;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = Herbalist.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class TickHandler {
+    public static int TICKS = 0;
+
+    @SubscribeEvent
+    public static void onTick(TickEvent.ServerTickEvent event) {
+        if (event.phase == TickEvent.Phase.START) {
+            TICKS++;
+        }
+    }
+}
+
